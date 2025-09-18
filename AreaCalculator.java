db59348d1129b49674242efb5b5de48648eab089
Nida Naz <naznida991@gmail.com>
2025-09-18 20:53:30 +0500

    AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -21,4 +21,17 @@ public class AreaCalculator {
     public double area(int side) {
         return side * side;
     }
+
+
+
+        public static void main(String[] args) {
+            AreaCalculator a=new AreaCalculator();
+            System.out.println("Area of Circle = "+a.area(5.9));
+            System.out.println("Area of Rectangle = "+a.area(5.9,8.6));
+            System.out.println("Area of square  = "+a.area(5));
+
+        }
+
+
 }
+
